9d08114d454c7c7d20a6947189855cb57f34fe8f
ahmed0hamri <7mid7imri@gmail.com>
2024-10-03 12:37:12 +0200

    task 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -4,3 +4,14 @@
 def add(a: float, b: float) -> float:
     
     return a + b
+#!/usr/bin/env python3
+"""
+Basic annotations - add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    function to add two floats
+    """
+    return a + b
